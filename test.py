0433489e24c1117b3f2542a24156ffa19e301e0d
labhansh2 <labhanshtimande4@gmail.com>
2023-05-13 06:55:24 +0530

    clean

--- test.py
+++
@@ -1,7 +0,0 @@
-# with open('chat_logs\.log_counter.txt', 'r') as get_count:
-#     data = get_count.read()
-
-# print(data)
-
-# with open('chat_logs\.log_counter.txt', 'w') as edit_count:
-#     edit_count.write(str(int(data) + 1))
